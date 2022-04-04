331c3f75139d10a9da371b4ffa1f26277897c65d
Daniele Bonini (皮夕): WebDev and DevOps by lots of Sim.pli.city bits <my25mb@aol.com>
2022-04-05 00:19:56 +0200

    Add files via upload

--- Private/scripts/home.php
+++ Private/scripts/home.php
@@ -1186,7 +1186,7 @@ function editparamValidation() {
   <meta name="author" content="5 Mode"/> 
   <meta name="robots" content="noindex"/>
   
-  <script src="./js/jquery-3.1.0.min.js" type="text/javascript"></script>
+  <script src="./js/jquery-3.6.0.min.js" type="text/javascript"></script>
   <script src="./js/common.js" type="text/javascript"></script>
   <script src="./js/bootstrap.min.js" type="text/javascript"></script>
   <script src="./js/sha.js" type="text/javascript"></script>
